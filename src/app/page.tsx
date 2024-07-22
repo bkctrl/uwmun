df67032c3782383ea52a9f044ca702aa1dcb7bb4
bkctrl <hb3kang@uwaterloo.ca>
2024-07-23 01:06:33 +0800

    final design update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import { Analytics } from "@vercel/analytics/react";
 export default function Home() {
   return (
     <div>
+      <Analytics/> 
+      <SpeedInsights/>
       <App />
     </div>
   );
