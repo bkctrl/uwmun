e18e330bdd4804d4a6f554b7d7de1b9893defeca
bkctrl <hb3kang@uwaterloo.ca>
2024-07-23 06:08:37 +0800

    final design update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 import { App } from "@/components/App";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "UWMUN",
-};
 
 export default function Home() {
   return (
     <div>
+      <title>UWMUN</title>
       <App />
       <Analytics/> 
       <SpeedInsights/>
