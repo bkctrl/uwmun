3da1cd9170da410b25632952732cdf875f51745b
bkctrl <hb3kang@uwaterloo.ca>
2024-07-23 04:53:15 +0800

    final design update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
         <title>UWMUN</title>
         <meta name="description" content="UWMUN - Participate in MUN conferences, improve your public speaking, and engage in global discussions at the University of Waterloo." />
         <meta name="keywords" content="University of Waterloo Model United Nations, University of Waterloo MUN, University of Waterloo, UW Model United Nations, UWMUN, uwmun, UW MUN, MUN, uwmun.ca, uwmun.com" />
+        <meta name="robots" content="index, follow" />
       </Head>
       <App />
       <Analytics/> 
