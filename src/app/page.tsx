74363e2d1b5034c24e3cb80c72cd3f8a63dc73b4
bkctrl <hb3kang@uwaterloo.ca>
2024-07-23 04:36:46 +0800

    final design update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import { App } from "@/components/App";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>UWMUN</title>
+        <meta name="description" content="UWMUN - Participate in MUN conferences, improve your public speaking, and engage in global discussions at the University of Waterloo." />
+        <meta name="keywords" content="University of Waterloo Model United Nations, University of Waterloo MUN, University of Waterloo, UW Model United Nations, UWMUN, uwmun, UW MUN, MUN, uwmun.ca, uwmun.com" />
+      </Head>
+      <App />
       <Analytics/> 
       <SpeedInsights/>
-      <App />
     </div>
   );
 }
