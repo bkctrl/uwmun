f3a8604a51dc4a123d6433be84ca380b3e5fcd90
bkctrl <hb3kang@uwaterloo.ca>
2024-07-24 19:27:56 +0800

    Implemeneted backend server

--- api/index.js
+++ api/index.js
@@ -1,18 +1,18 @@
 import express from "express";
 import { Client } from "@notionhq/client";
-const app = express();
 import 'dotenv/config';
 import cors from "cors";
   
-const notionKey = process.env.NEXT_PUBLIC_NOTION_API_KEY!;
-const execsDB = process.env.NEXT_PUBLIC_NOTION_EXECUTIVES_DATABASE_ID!;
-const eventsDB = process.env.NEXT_PUBLIC_NOTION_EVENTS_DATABASE_ID!;
-const resourcesDB = process.env.NEXT_PUBLIC_NOTION_RESOURCES_DATABASE_ID!;
+const notionKey = process.env.NEXT_PUBLIC_NOTION_API_KEY;
+const execsDB = process.env.NEXT_PUBLIC_NOTION_EXECUTIVES_DATABASE_ID;
+const eventsDB = process.env.NEXT_PUBLIC_NOTION_EVENTS_DATABASE_ID;
+const resourcesDB = process.env.NEXT_PUBLIC_NOTION_RESOURCES_DATABASE_ID;
 
 const notion = new Client({ 
   auth: notionKey,
 });
 
+const app = express();
 app.use(cors());
 
 app.get('/execs-data', async (req, res) => { 
@@ -21,11 +21,9 @@ app.get('/execs-data', async (req, res) => {
     const response = await notion.databases.query({
       database_id: execsDB
     }); 
-    //@ts-ignore
     const stringy = response.results.reverse().map(result => result.properties)
     res.json(stringy);
   } catch (error) {
-    //@ts-ignore
     res.status(500).json({ error: error.message });  
   } 
 }); 
@@ -36,11 +34,9 @@ app.get('/events-data', async (req, res) => {
     const response = await notion.databases.query({
       database_id: eventsDB
     }); 
-    //@ts-ignore
     const stringy = response.results.reverse().map(result => result.properties)
     res.json(stringy);
   } catch (error) {
-    //@ts-ignore
     res.status(500).json({ error: error.message });  
   } 
 }); 
@@ -51,17 +47,15 @@ app.get('/resources-data', async (req, res) => {
     const response = await notion.databases.query({
       database_id: resourcesDB
     }); 
-    //@ts-ignore
     const stringy = response.results.reverse().map(result => result.properties)
     res.json(stringy);
   } catch (error) {
-    //@ts-ignore
     res.status(500).json({ error: error.message });  
   } 
 }); 
 
-app.listen(4000, () => {
-    console.log('Server is running on port 4000');
-});
+// app.listen(4000, () => {
+//     console.log('Server is running on port 4000');
+// });
 
-export default app;
+module.exports = app;
